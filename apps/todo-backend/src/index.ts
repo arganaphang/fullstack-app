a325df751d858b187134d16e79a4a48ef9146856
Argana Phangquestian <arganaphangquestian@gmail.com>
2024-12-28 01:34:32 +0700

    update: add cors middleware update: update response conventions

--- apps/todo-backend/src/index.ts
+++ apps/todo-backend/src/index.ts
@@ -1,42 +1,43 @@
 import {
-	createTodo,
-	deleteTodo,
-	getTodos,
-	toggleCompletedTodo,
-	updateTodo,
+  createTodo,
+  deleteTodo,
+  getTodos,
+  toggleCompletedTodo,
 } from "@/todo";
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { cors } from 'hono/cors'
 
 const app = new Hono();
+app.use('/*', cors());
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+  return c.text("Hello Hono!");
 });
 
 // Route Todos
 const todos = new Hono();
 
 todos.get("/", async (c) => {
-	return c.json({ todos: await getTodos(), message: "get all todo" });
+  return c.json({ data: await getTodos(), message: "get all todo" });
 });
 
 todos.post("/", async (c) => {
-	const { title } = await c.req.json();
-	const todo = (await createTodo(title))?.[0];
-	return c.json({ message: "todo created", todo });
+  const { title } = await c.req.json();
+  const todo = (await createTodo(title))?.[0];
+  return c.json({ message: "todo created", data: todo });
 });
 
 todos.patch("/:id", async (c) => {
-	const { id } = c.req.param();
-	const todo = (await toggleCompletedTodo(Number(id)))?.[0];
-	return c.json({ message: "todo updated", todo });
+  const { id } = c.req.param();
+  const todo = (await toggleCompletedTodo(Number(id)))?.[0];
+  return c.json({ message: "todo updated", data: todo });
 });
 
 todos.delete("/:id", async (c) => {
-	const { id } = c.req.param();
-	await deleteTodo(Number(id));
-	return c.json({ message: "todo deleted" });
+  const { id } = c.req.param();
+  await deleteTodo(Number(id));
+  return c.json({ message: "todo deleted" });
 });
 
 app.route("/todos", todos);
@@ -45,6 +46,6 @@ const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
 
 serve({
-	fetch: app.fetch,
-	port,
+  fetch: app.fetch,
+  port,
 });
--- apps/todo-backend/src/todo.ts
+++ apps/todo-backend/src/todo.ts
@@ -1,35 +1,35 @@
 import { db } from "@/db/db";
 import { todos } from "@/db/schema/todo";
-import { eq, not } from "drizzle-orm";
+import { eq, not, asc } from "drizzle-orm";
 
 export const getTodos = async () => {
-	return db.select().from(todos);
+  return db.select().from(todos).orderBy(asc(todos.id));
 };
 
 export const getTodo = async (id: number) => {
-	const todo = await db.select().from(todos).where(eq(todos.id, id)).limit(1);
-	return todo[0];
+  const todo = await db.select().from(todos).where(eq(todos.id, id)).limit(1);
+  return todo[0];
 };
 
 export const createTodo = async (title: string) => {
-	return db.insert(todos).values({ title }).returning();
+  return db.insert(todos).values({ title }).returning();
 };
 
 export const updateTodo = async (id: number, title: string) => {
-	return db
-		.update(todos)
-		.set({ title, completed: not(todos.completed) })
-		.where(eq(todos.id, id));
+  return db
+    .update(todos)
+    .set({ title, completed: not(todos.completed) })
+    .where(eq(todos.id, id));
 };
 
 export const toggleCompletedTodo = async (id: number) => {
-	return db
-		.update(todos)
-		.set({ completed: not(todos.completed) })
-		.where(eq(todos.id, id))
-		.returning();
+  return db
+    .update(todos)
+    .set({ completed: not(todos.completed) })
+    .where(eq(todos.id, id))
+    .returning();
 };
 
 export const deleteTodo = async (id: number) => {
-	return db.delete(todos).where(eq(todos.id, id));
+  return db.delete(todos).where(eq(todos.id, id));
 };
